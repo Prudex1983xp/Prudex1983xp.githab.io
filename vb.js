829cc7813fcdbf2c06ca232552696f508b008461
Prudex1983xp <105013402+Prudex1983xp@users.noreply.github.com>
2022-05-06 23:04:56 +0500

    Add files via upload

---
+++ vb.js
@@ -0,0 +1,19 @@
+let obj = [
+{
+    "color": "gren",
+    "vozrast": ["15","10"],
+    "name": true
+},
+{
+    "color": "red",
+    "vozrast": ["10","15"],
+    "name": true
+},
+{
+    "color": "orange",
+    "vozrast": ["5","23"],
+    "name": true
+}
+]  
+console.log(obj)
+
